a40e4fd783d94079faeb21997f53e797214e29e0
mohamed fatta <medfata3@gmail.com>
2020-11-10 12:53:09 +0000

    1 adding routes

--- chatAppClientSide/src/app/app.module.ts
+++ chatAppClientSide/src/app/app.module.ts
@@ -1,16 +1,32 @@
+import { HomePageComponent } from './home-page/home-page.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HeaderComponent } from './header/header.component';
+import { RouterModule } from '@angular/router';
+import { MainComponent } from './main/main.component';
+import { FooterComponent } from './footer/footer.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    FooterComponent,
+    WelcomeComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
-    NgbModule
+    NgbModule,
+    RouterModule.forRoot([
+      {path:"welcome",component:WelcomeComponent},
+      {path:"home-page", component:HomePageComponent},
+      { path: "**", redirectTo: "/welcome" }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
